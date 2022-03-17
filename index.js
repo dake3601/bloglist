341e9bbd2da0e1bb0478e73a1ffa45a35a0502dc
Keven Arroyo <dake.3601@gmail.com>
2022-03-16 18:43:34 -0600

    Added models

--- index.js
+++ index.js
@@ -2,21 +2,9 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
 const { info, error } = require('./utils/logger')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
+const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
