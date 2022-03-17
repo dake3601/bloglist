c31b63c28bcf73f247629eb4d0324b800e296c4a
Keven Arroyo <dake.3601@gmail.com>
2022-03-16 19:15:01 -0600

    Fix - Adding back cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
 const blogSchema = require('./controllers/blogs')
 
