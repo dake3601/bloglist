48d8effb63b9535af8c10ed94d0b5d97a93022c9
Keven Arroyo <dake.3601@gmail.com>
2022-03-19 10:26:49 -0600

    Setting up test environment

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,4 +1,5 @@
 const listHelper = require('../utils/list_helper')
+const helper = require('./test_helper')
 
 test('dummy returns one', () => {
   const blogs = []
@@ -6,87 +7,6 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
-
-const listWithOneBlog = [
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  }
-]
-
-const listWithBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
-  }
-]
-
-const twoFavorites = [
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 5,
-    __v: 0
-  }
-]
 
 describe('total likes', () => {
 
@@ -96,12 +16,12 @@ describe('total likes', () => {
   })
 
   test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
+    const result = listHelper.totalLikes(helper.listWithOneBlog)
     expect(result).toBe(5)
   })
 
   test('when list has many blogs, equals sum', () => {
-    const result = listHelper.totalLikes(listWithBlogs)
+    const result = listHelper.totalLikes(helper.listWithBlogs)
     expect(result).toBe(36)
   })
 })
@@ -114,7 +34,7 @@ describe('favorite blog', () => {
   })
 
   test('when list has only one blog, equals that blog', () => {
-    const result = listHelper.favoriteBlog(listWithOneBlog)
+    const result = listHelper.favoriteBlog(helper.listWithOneBlog)
     expect(result).toEqual({
       title: 'Go To Statement Considered Harmful',
       author: 'Edsger W. Dijkstra',
@@ -123,7 +43,7 @@ describe('favorite blog', () => {
   })
 
   test('when list has many blogs, equals favorite', () => {
-    const result = listHelper.favoriteBlog(listWithBlogs)
+    const result = listHelper.favoriteBlog(helper.listWithBlogs)
     expect(result).toEqual({
       title: 'Canonical string reduction',
       author: 'Edsger W. Dijkstra',
@@ -132,7 +52,7 @@ describe('favorite blog', () => {
   })
 
   test('when list has many blogs, equals one of the two', () => {
-    const result = listHelper.favoriteBlog(twoFavorites)
+    const result = listHelper.favoriteBlog(helper.twoFavorites)
     try {
       expect(result).toEqual({
         title: 'Canonical string reduction',
@@ -157,7 +77,7 @@ describe('most blogs', () => {
   })
 
   test('when list has only one blog, equals that author', () => {
-    const result = listHelper.mostBlogs(listWithOneBlog)
+    const result = listHelper.mostBlogs(helper.listWithOneBlog)
     expect(result).toEqual({
       author: 'Edsger W. Dijkstra',
       blogs: 1
@@ -165,7 +85,7 @@ describe('most blogs', () => {
   })
 
   test('when list has many blogs, equals top author', () => {
-    const result = listHelper.mostBlogs(listWithBlogs)
+    const result = listHelper.mostBlogs(helper.listWithBlogs)
     expect(result).toEqual({
       author: 'Robert C. Martin',
       blogs: 3
@@ -173,7 +93,7 @@ describe('most blogs', () => {
   })
 
   test('when list has two equal authors, equals one of the two', () => {
-    const result = listHelper.mostBlogs(twoFavorites)
+    const result = listHelper.mostBlogs(helper.twoFavorites)
     try {
       expect(result).toEqual({
         author: 'Edsger W. Dijkstra',
@@ -196,7 +116,7 @@ describe('most likes', () => {
   })
 
   test('when list has only one blog, equals that author', () => {
-    const result = listHelper.mostLikes(listWithOneBlog)
+    const result = listHelper.mostLikes(helper.listWithOneBlog)
     expect(result).toEqual({
       author: 'Edsger W. Dijkstra',
       likes: 5
@@ -204,7 +124,7 @@ describe('most likes', () => {
   })
 
   test('when list has many blogs, equals top author', () => {
-    const result = listHelper.mostLikes(listWithBlogs)
+    const result = listHelper.mostLikes(helper.listWithBlogs)
     expect(result).toEqual({
       author: 'Edsger W. Dijkstra',
       likes: 17
@@ -212,7 +132,7 @@ describe('most likes', () => {
   })
 
   test('when list has two authors with equal likes, equals one of the two', () => {
-    const result = listHelper.mostLikes(twoFavorites)
+    const result = listHelper.mostLikes(helper.twoFavorites)
     try {
       expect(result).toEqual({
         author: 'Edsger W. Dijkstra',
