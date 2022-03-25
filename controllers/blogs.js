ff73b90fc9ef0012d0d6b744b22b0e676e03a54d
Keven Arroyo <dake.3601@gmail.com>
2022-03-24 19:01:20 -0600

    Part 4.21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -34,8 +34,19 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog.user.toString() === decodedToken.id.toString()) {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } else {
+    return response.status(401).json({ error: 'invalid username or password' })
+  }
 })
 
 blogsRouter.put('/:id', async (request, response, next) => {
